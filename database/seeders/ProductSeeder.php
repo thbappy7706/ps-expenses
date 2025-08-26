63fcc22f08f312b959d66f8a883e4676e873475e
thbappy7706 <tanvir15-7706@diu.edu.bd>
2025-08-26 20:50:48 +0600

    --

--- database/seeders/ProductSeeder.php
+++ database/seeders/ProductSeeder.php
@@ -4,6 +4,7 @@ namespace Database\Seeders;
 
 use App\Jobs\SeedProductsJob;
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\Artisan;
 
 class ProductSeeder extends Seeder
 {
@@ -14,14 +15,16 @@ class ProductSeeder extends Seeder
     {
         $totalProducts = 100000; // 1 lakh products
         $batchSize = 1000; // Process 1000 products per job
-        
+
         $batches = ceil($totalProducts / $batchSize);
-        
+
         for ($i = 0; $i < $batches; $i++) {
             $remainingProducts = $totalProducts - ($i * $batchSize);
             $currentBatchSize = min($batchSize, $remainingProducts);
-            
+
             SeedProductsJob::dispatch($currentBatchSize, $i * $batchSize);
         }
+
+        Artisan::call('queue:work --stop-when-empty');
     }
 }
